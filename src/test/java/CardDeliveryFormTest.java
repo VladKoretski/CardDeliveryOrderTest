ddc964bf0edb25d3bbc7ce3447fa90d8302ddc57
Vladko1973 <vpk1973@yandex.ru>
2023-08-08 09:28:18 +0300

    first commit

---
+++ src/test/java/CardDeliveryFormTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+class CardDeliveryFormTest {
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    void shouldTest() {
+        open("http://localhost:7777");
+        $("[data-test-id='city'] input").setValue("Ижевск");
+        String currentDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.LEFT_SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(currentDate);
+        $("[data-test-id='name'] input").setValue("Корецкий Владимир");
+        $("[data-test-id='phone'] input").setValue("+79333333333");
+        $("[data-test-id='agreement']").click();
+        sleep(1000);
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + currentDate));
+    }
+}
