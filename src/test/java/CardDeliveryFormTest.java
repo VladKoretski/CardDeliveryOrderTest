dc3fd7cda2e82fdcd31642dd7605d691a20ca427
Vladko1973 <vpk1973@yandex.ru>
2023-08-10 19:56:45 +0300

    goodtestnew

--- src/test/java/CardDeliveryFormTest.java
+++ src/test/java/CardDeliveryFormTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -7,7 +8,8 @@ import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 class CardDeliveryFormTest {
 
@@ -15,9 +17,13 @@ class CardDeliveryFormTest {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
+    @BeforeEach
+    void setup() {
+        open("http://localhost:7777");
+    }
+
     @Test
     void shouldTest() {
-        open("http://localhost:7777");
         $("[data-test-id='city'] input").setValue("Ижевск");
         String currentDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.LEFT_SHIFT, Keys.HOME), Keys.DELETE);
@@ -25,7 +31,6 @@ class CardDeliveryFormTest {
         $("[data-test-id='name'] input").setValue("Корецкий Владимир");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         $("[data-test-id='agreement']").click();
-        sleep(1000);
         $("button.button").click();
         $(".notification__content")
                 .shouldBe(visible, Duration.ofSeconds(15))
